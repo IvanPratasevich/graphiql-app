4800dc91df16eb3a74ed2be780e4abe6f20df2cb
IvanPratasevich <iv.pratasevich@gmail.com>
2023-05-24 17:19:48 +0300

    feat: add animation

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -7,10 +7,27 @@ const Logo: FC = () => {
   const { hovered, ref } = useHover();
   const intervalRef = useRef<number | NodeJS.Timer>(0);
   const [degree, setDegree] = useState(0);
-  const animationDuration = 5; //in seconds
+  const [pageLoaded, setPageLoaded] = useState(false);
+  const firstElectron = {
+    x: 37.5,
+    y: 17.843,
+    radius: 2.5,
+    cx: 24.97,
+    cy: 24.986,
+    rx: 22.534,
+    ry: 8.593,
+    angle: 0,
+  };
+
+  firstElectron.angle = -1 * Math.acos((firstElectron.x - firstElectron.cx) / firstElectron.rx);
+
+  const [positions, setPositions] = useState(firstElectron);
+
+  window.addEventListener('load', () => setPageLoaded(true));
 
   useEffect(() => {
     if (hovered) {
+      const animationDuration = 5; //in seconds
       const tempInterval = setInterval(() => {
         setDegree((prevDegree) => {
           const nextDegree = prevDegree + 1;
@@ -23,6 +40,32 @@ const Logo: FC = () => {
       clearInterval(intervalRef.current);
     };
   }, [hovered, degree]);
+
+  // Parametric equation of an ellipse x^2 / a^2 + y^2 / b^2 = 1
+  // --
+  // | x = a * cos(phi)   // phi - angle
+  // |
+  // | y = b * sin(phi)   // phi - angle
+  // --
+
+  useEffect(() => {
+    let requestId: number;
+    if (pageLoaded) {
+      const animate = (): void => {
+        const { rx, ry, cx, cy, angle } = positions;
+        const speed = 0.05; // radian
+        const newAngle = angle + speed; // radian
+        const x = cx + rx * Math.cos(newAngle); // rx - ellipse horizontal radius
+        const y = cy + ry * Math.sin(newAngle); // rx - ellipse vertical radius
+        setPositions((prev) => {
+          return { ...prev, x, y, angle: newAngle };
+        });
+      };
+      requestId = requestAnimationFrame(animate);
+    }
+
+    return () => cancelAnimationFrame(requestId ? requestId : 0);
+  }, [positions, pageLoaded]);
 
   return (
     <a
@@ -65,6 +108,12 @@ const Logo: FC = () => {
           style={{ fill: 'none', stroke: '#ffee08', strokeWidth: '2.019' }}
           d="M47,24.593c0,4.743-9.858,8.593-22,8.593c-12.142,0-22-3.85-22-8.593C3,19.85,12.858,16,25,16C37.142,16,47,19.85,47,24.593z"
         />
+        <circle
+          fill="url(#electron_first)"
+          cx={positions.x}
+          cy={positions.y}
+          r={positions.radius}
+        ></circle>
         <ellipse
           transform="matrix(0.866 -0.5 0.5 0.866 -9.1606 15.8414)"
           style={{ fill: 'none', stroke: '#ffee08', strokeWidth: 2 }}
@@ -73,6 +122,12 @@ const Logo: FC = () => {
           rx="8.593"
           ry="22.534"
         />
+        <defs>
+          <radialGradient id="electron_first" cx="50%" cy="50%" r="50%" fx="50%" fy="50%">
+            <stop offset="0%" stopColor="#d71e49" />
+            <stop offset="100%" stopColor="#ffff" />
+          </radialGradient>
+        </defs>
       </svg>
       <Title order={1}>GraphiQL</Title>
     </a>
